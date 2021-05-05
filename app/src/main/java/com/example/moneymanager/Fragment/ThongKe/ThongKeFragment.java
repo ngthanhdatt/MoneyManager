a4e626e5fc2f56eaf8696e31f5997fe6ac19ea01
assassin15141312 <75314930+assassin15141312@users.noreply.github.com>
2021-05-06 01:41:19 +0700

    gd_thong_ke

--- app/src/main/java/com/example/moneymanager/Fragment/ThongKe/ThongKeFragment.java
+++ app/src/main/java/com/example/moneymanager/Fragment/ThongKe/ThongKeFragment.java
@@ -1,25 +1,45 @@
 package com.example.moneymanager.Fragment.ThongKe;
 
+import android.annotation.SuppressLint;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.viewpager2.widget.ViewPager2;
+import androidx.appcompat.app.AppCompatActivity;
 
+
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.DatePicker;
 
 import com.example.moneymanager.Adapter.FragmentThongKeAdapter;
 import com.example.moneymanager.R;
 import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
 public class ThongKeFragment extends Fragment {
 
     private TabLayout tabLayout;
     private ViewPager2 viewPager2;
+    private Toolbar toolbar;
+    private Button start;
+    private Button end;
+    private DatePicker datePicker;
+    private int year, month, date;
+
 
     public ThongKeFragment() {
         // Required empty public constructor
@@ -40,6 +60,68 @@ public class ThongKeFragment extends Fragment {
         viewPager2 = view.findViewById(R.id.frag_viewPager_thongke);
 
 
+        toolbar = (Toolbar)view.findViewById(R.id.frag_Toolbar_thongke);
+        ((AppCompatActivity)getActivity()).setSupportActionBar(toolbar);
+        ((AppCompatActivity)getActivity()).getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+
+        start = (Button)view.findViewById(R.id.frag_Toolbar_thongke_StartDate);
+        end = (Button)view.findViewById(R.id.frag_Toolbar_thongke_EndDate);
+
+
+        Calendar cal = Calendar.getInstance();
+        int last_day = cal.getActualMaximum(Calendar.DATE);
+        String currentTime = new SimpleDateFormat("MM-yyyy", Locale.getDefault()).format(new Date());
+        start.setText("01-" +currentTime);
+        end.setText(Integer.toString(last_day)+'-'+currentTime);
+
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Calendar c = Calendar.getInstance();
+                year = c.get(Calendar.YEAR);
+                month = c.get(Calendar.MONTH);
+                date = c.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog datePickerDialog = new DatePickerDialog(viewPager2.getContext(),
+                        new DatePickerDialog.OnDateSetListener() {
+
+                            @Override
+                            public void onDateSet(DatePicker view, int year,
+                                                  int monthOfYear, int dayOfMonth) {
+
+                                start.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
+
+                            }
+                        }, year, month, date);
+                datePickerDialog.show();
+            }
+        });
+
+        end.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Calendar c = Calendar.getInstance();
+                year = c.get(Calendar.YEAR);
+                month = c.get(Calendar.MONTH);
+                date = c.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog datePickerDialog = new DatePickerDialog(viewPager2.getContext(),
+                        new DatePickerDialog.OnDateSetListener() {
+
+                            @Override
+                            public void onDateSet(DatePicker view, int year,
+                                                  int monthOfYear, int dayOfMonth) {
+
+                                end.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
+
+                            }
+                        }, year, month, date);
+                datePickerDialog.show();
+            }
+        });
+
+
         FragmentThongKeAdapter fragmentThongKeAdapter = new FragmentThongKeAdapter(this.getActivity());
         viewPager2.setAdapter(fragmentThongKeAdapter);
 
@@ -53,5 +135,7 @@ public class ThongKeFragment extends Fragment {
                 }
             }
         }).attach();
+
     }
+
 }

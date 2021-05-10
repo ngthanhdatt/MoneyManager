318d49b9dba8ed043ec88ec7fc17e59a559882c6
assassin15141312 <75314930+assassin15141312@users.noreply.github.com>
2021-05-10 17:46:00 +0700

    gd_cai_dat

--- app/src/main/java/com/example/moneymanager/Fragment/CaiDatFragment.java
+++ app/src/main/java/com/example/moneymanager/Fragment/CaiDatFragment.java
@@ -2,11 +2,17 @@ package com.example.moneymanager.Fragment;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.moneymanager.R;
 
@@ -62,5 +68,43 @@ public class CaiDatFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_cai_dat, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        Toolbar toolbar1 =(Toolbar)view.findViewById(R.id.frag_Toolbar1_caidat);
+        Toolbar toolbar2 =(Toolbar)view.findViewById(R.id.frag_Toolbar2_caidat);
+        Toolbar toolbar3 =(Toolbar)view.findViewById(R.id.frag_Toolbar3_caidat);
+        Toolbar toolbar4 =(Toolbar)view.findViewById(R.id.frag_Toolbar4_caidat);
+
+        TextView textView = view.findViewById((R.id.frag_Toolbar_caidat_TextView));
+
+        Button caidat = view.findViewById((R.id.frag_viewPager_caidat_CaiDat));
+        Button vitien = view.findViewById((R.id.frag_viewPager_caidat_ViTien));
+        Button matkhau = view.findViewById((R.id.frag_viewPager_caidat_MatKhau));
+        Button saoluu = view.findViewById((R.id.frag_viewPager_caidat_SaoLuu));
+        Button phongcach = view.findViewById((R.id.frag_viewPager_caidat_PhongCach));
+        Button phanhoi = view.findViewById((R.id.frag_viewPager_caidat_PhanHoi));
+        Button trogiup = view.findViewById((R.id.frag_viewPager_caidat_TroGiup));
+        Button gioithieu = view.findViewById((R.id.frag_viewPager_caidat_GioiThieu));
+
+
+        phongcach.setOnClickListener(new View.OnClickListener() {
+            int i=0;
+            @Override
+            public void onClick(View v) {
+                if(i%2==0) {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                    i += 1;
+                }
+                else{
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                    i+=1;
+                }
+
+            }
+        });
     }
 }

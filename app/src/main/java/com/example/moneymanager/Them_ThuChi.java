99871d31da9479fd7a9ca46a3f4b25e5926d5272
assassin15141312 <75314930+assassin15141312@users.noreply.github.com>
2021-05-12 15:33:51 +0700

    gd_them_thu_chi_2

--- app/src/main/java/com/example/moneymanager/Them_ThuChi.java
+++ app/src/main/java/com/example/moneymanager/Them_ThuChi.java
@@ -1,14 +1,15 @@
 package com.example.moneymanager;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
@@ -16,6 +17,10 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.TimePicker;
 
+import com.example.moneymanager.Fragment.ThuChi.BottomSheetDialog_LoaiChi;
+import com.example.moneymanager.Fragment.ThuChi.BottomSheetDialog_LoaiThu;
+import com.example.moneymanager.Fragment.ThuChi.BottomSheetDialog_ViTien;
+import com.google.android.material.tabs.TabItem;
 import com.google.android.material.tabs.TabLayout;
 
 import java.text.ParseException;
@@ -26,6 +31,7 @@ import java.util.Locale;
 
 public class Them_ThuChi extends AppCompatActivity {
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,32 +39,34 @@ public class Them_ThuChi extends AppCompatActivity {
 
         Toolbar toolbar1 = findViewById(R.id.frag_Toolbar_ThemThuChi);
         Toolbar toolbar2 = findViewById(R.id.frag_Toolbar_ThemThuChi_ngay);
-        Toolbar toolbar3 = findViewById(R.id.frag_Toolbar_ThemThuChi_TaiKhoan);
+        Toolbar toolbar3 = findViewById(R.id.frag_Toolbar_ThemThuChi_ViTien);
         Toolbar toolbar4 = findViewById(R.id.frag_Toolbar_ThemThuChi_TheLoai);
         Toolbar toolbar5 = findViewById(R.id.frag_Toolbar_ThemThuChi_SoTien);
         Toolbar toolbar6 = findViewById(R.id.frag_Toolbar_ThemThuChi_GhiChu);
 
         Button back = findViewById(R.id.frag_Toolbar_ThemThuChi_back);
+        back.setText("Thu");
         Button luu = findViewById(R.id.button_themthuchi);
         Button tieptuc = findViewById(R.id.button_tieptuc);
         Button editNgay = findViewById(R.id.edit_Ngay);
         Button editGio = findViewById(R.id.edit_Gio);
 
         TextView ngay = findViewById(R.id.TextView_ngay);
-        TextView taikhoan = findViewById(R.id.TextView_taikhoan);
+        TextView vitien = findViewById(R.id.TextView_vitien);
         TextView theloai = findViewById(R.id.TextView_theloai);
         TextView sotien = findViewById(R.id.TextView_sotien);
         TextView ghichu = findViewById(R.id.TextView_ghichu);
 
 
-        EditText editTaikhoan = findViewById(R.id.editText_taikhoan);
+        EditText editVitien = findViewById(R.id.editText_vitien);
         EditText editTheloai = findViewById(R.id.editText_theloai);
         EditText editSotien = findViewById(R.id.editText_sotien);
         EditText editGhichu = findViewById(R.id.editText_ghichu);
 
         TabLayout tabLayout = findViewById(R.id.frag_tabLayout_ThemThuChi);
-        tabLayout.addTab(tabLayout.newTab().setText("Thu"));
-        tabLayout.addTab(tabLayout.newTab().setText("Chi"));
+        TabItem tabThu= findViewById(R.id.tabThu);
+        TabItem tabChi= findViewById(R.id.tabChi);
+
 
         String currentDate = new SimpleDateFormat("dd/MM/yyyy(EEE)", Locale.getDefault()).format(new Date());
         editNgay.setText(currentDate);
@@ -125,6 +133,60 @@ public class Them_ThuChi extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(v.getContext(), MainActivity.class);
                 v.getContext().startActivity(intent);
+            }
+        });
+
+
+        editVitien.setInputType(InputType.TYPE_NULL);
+        //editVitien.setShowSoftInputOnFocus(false);
+        editVitien.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                BottomSheetDialog_ViTien bottomSheet = new BottomSheetDialog_ViTien();
+                bottomSheet.show(getSupportFragmentManager(),
+                        "ModalBottomSheet");
+            }
+        });
+
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                if(tab.getPosition()==0) {
+                    back.setText("Thu");
+                }
+                if(tab.getPosition()==1) {
+                    back.setText("Chi");
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+
+        editTheloai.setInputType(InputType.TYPE_NULL);
+        //editTheloai.setShowSoftInputOnFocus(false);
+        editTheloai.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(back.getText()=="Thu") {
+                    BottomSheetDialog_LoaiThu bottomSheet = new BottomSheetDialog_LoaiThu();
+                    bottomSheet.show(getSupportFragmentManager(),
+                            "ModalBottomSheet");
+                }
+                if(back.getText()=="Chi") {
+                    BottomSheetDialog_LoaiChi bottomSheet = new BottomSheetDialog_LoaiChi();
+                    bottomSheet.show(getSupportFragmentManager(),
+                            "ModalBottomSheet");
+                }
             }
         });
     }

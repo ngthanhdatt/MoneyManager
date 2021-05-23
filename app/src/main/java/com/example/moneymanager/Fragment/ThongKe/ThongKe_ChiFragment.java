b33be1fc8593711782a9916ead42399bf8542d0c
assassin15141312 <75314930+assassin15141312@users.noreply.github.com>
2021-05-23 22:58:07 +0700

    thong ke

--- app/src/main/java/com/example/moneymanager/Fragment/ThongKe/ThongKe_ChiFragment.java
+++ app/src/main/java/com/example/moneymanager/Fragment/ThongKe/ThongKe_ChiFragment.java
@@ -1,14 +1,28 @@
 package com.example.moneymanager.Fragment.ThongKe;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.example.moneymanager.Database.DatabaseHelper;
+import com.example.moneymanager.Model.Chi;
+import com.example.moneymanager.Model.Thu;
 import com.example.moneymanager.R;
+
+import org.eazegraph.lib.charts.PieChart;
+import org.eazegraph.lib.models.PieModel;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -16,6 +30,9 @@ import com.example.moneymanager.R;
  * create an instance of this fragment.
  */
 public class ThongKe_ChiFragment extends Fragment {
+
+    TextView tvAnUong, tvGiaiTri, tvSoThich, tvGiaoThong,tvSinhHoat,tvAoQuan,tvLamDep,tvSucKhoe,tvGiaoDuc,tvSuKien,tvKhac;
+    PieChart pieChart;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -62,5 +79,151 @@ public class ThongKe_ChiFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_thong_ke__chi, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull @NotNull View view, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        tvAnUong=view.findViewById(R.id.tvAnUong);
+        tvGiaiTri=view.findViewById(R.id.tvGiaiTri);
+        tvSoThich=view.findViewById(R.id.tvSoThich);
+        tvGiaoThong=view.findViewById(R.id.tvGiaoThong);
+        tvSinhHoat=view.findViewById(R.id.tvSinhHoat);
+        tvAoQuan=view.findViewById(R.id.tvAoQuan);
+        tvLamDep=view.findViewById(R.id.tvLamDep);
+        tvSucKhoe=view.findViewById(R.id.tvSucKhoe);
+        tvGiaoDuc=view.findViewById(R.id.tvGiaoDuc);
+        tvSuKien=view.findViewById(R.id.tvSuKien);
+        tvKhac=view.findViewById(R.id.tvKhac_Chi);
+
+
+        pieChart = view.findViewById(R.id.piechart_chi);
+        setData();
+    }
+
+    private void setData(){
+        DatabaseHelper db= new DatabaseHelper(getContext());
+        List<Chi> list = new ArrayList<>();
+        list=db.getAllChi();
+        int anuong=0;
+        int giaitri=0;
+        int sothich=0;
+        int sinhhoat=0;
+        int giaothong=0;
+        int aoquan=0;
+        int lamdep=0;
+        int giaoduc=0;
+        int suckhoe=0;
+        int sukien=0;
+        int khac=0;
+
+        for(int i=0;i<list.size();i++){
+            if(list.get(i).getLoaiChi().getId()==db.getLoaiChiByName("Ăn uống").getId()){
+                anuong+= list.get(i).getSotien();
+            }
+            if(list.get(i).getLoaiChi().getId()==db.getLoaiChiByName("Giải trí").getId()){
+                giaitri+= list.get(i).getSotien();
+            }
+            if(list.get(i).getLoaiChi().getId()==db.getLoaiChiByName("Sinh hoạt").getId()){
+                sinhhoat+= list.get(i).getSotien();
+            }
+            if(list.get(i).getLoaiChi().getId()==db.getLoaiChiByName("Sở thích").getId()){
+                sothich+= list.get(i).getSotien();
+            }
+            if(list.get(i).getLoaiChi().getId()==db.getLoaiChiByName("Giao thông").getId()){
+                giaothong+= list.get(i).getSotien();
+            }
+            if(list.get(i).getLoaiChi().getId()==db.getLoaiChiByName("Áo quần").getId()){
+                aoquan+= list.get(i).getSotien();
+            }
+            if(list.get(i).getLoaiChi().getId()==db.getLoaiChiByName(" Làm đẹp").getId()){
+                lamdep+= list.get(i).getSotien();
+            }
+            if(list.get(i).getLoaiChi().getId()==db.getLoaiChiByName("Sự kiện").getId()){
+                sukien+= list.get(i).getSotien();
+            }
+            if(list.get(i).getLoaiChi().getId()==db.getLoaiChiByName("Giáo dục").getId()){
+                giaoduc+= list.get(i).getSotien();
+            }
+            if(list.get(i).getLoaiChi().getId()==db.getLoaiChiByName("Sức khỏe").getId()){
+                suckhoe+= list.get(i).getSotien();
+            }
+            if(list.get(i).getLoaiChi().getId()==db.getLoaiChiByName("Khác").getId()){
+                khac+= list.get(i).getSotien();
+            }
+
+        }
+        tvAnUong.setText(Integer.toString(anuong));
+        tvGiaiTri.setText(Integer.toString(giaitri));
+        tvSoThich.setText(Integer.toString(sothich));
+        tvGiaoThong.setText(Integer.toString(giaothong));
+        tvSinhHoat.setText(Integer.toString(sinhhoat));
+        tvAoQuan.setText(Integer.toString(aoquan));
+        tvLamDep.setText(Integer.toString(lamdep));
+        tvSucKhoe.setText(Integer.toString(suckhoe));
+        tvGiaoDuc.setText(Integer.toString(giaoduc));
+        tvSuKien.setText(Integer.toString(sukien));
+        tvKhac.setText(Integer.toString(khac));
+
+
+        pieChart.addPieSlice(
+                new PieModel(
+                        "tvAnUong",
+                        Integer.parseInt(tvAnUong.getText().toString()),
+                        Color.parseColor("#ff0000")));
+        pieChart.addPieSlice(
+                new PieModel(
+                        "tvGiaiTri",
+                        Integer.parseInt(tvGiaiTri.getText().toString()),
+                        Color.parseColor("#0000ff")));
+        pieChart.addPieSlice(
+                new PieModel(
+                        "tvSoThich",
+                        Integer.parseInt(tvSoThich.getText().toString()),
+                        Color.parseColor("#ffff00")));
+        pieChart.addPieSlice(
+                new PieModel(
+                        "tvGiaoThong",
+                        Integer.parseInt(tvGiaoThong.getText().toString()),
+                        Color.parseColor("#ff00ff")));
+        pieChart.addPieSlice(
+                new PieModel(
+                        "tvSinhHoat",
+                        Integer.parseInt(tvSinhHoat.getText().toString()),
+                        Color.parseColor("#ff00ff")));
+        pieChart.addPieSlice(
+                new PieModel(
+                        "tvAoQuan",
+                        Integer.parseInt(tvAoQuan.getText().toString()),
+                        Color.parseColor("#ff00ff")));
+        pieChart.addPieSlice(
+                new PieModel(
+                        "tvLamDep",
+                        Integer.parseInt(tvLamDep.getText().toString()),
+                        Color.parseColor("#ff00ff")));
+        pieChart.addPieSlice(
+                new PieModel(
+                        "tvSucKhoe",
+                        Integer.parseInt(tvSucKhoe.getText().toString()),
+                        Color.parseColor("#ff00ff")));
+        pieChart.addPieSlice(
+                new PieModel(
+                        "tvGiaoDuc",
+                        Integer.parseInt(tvGiaoDuc.getText().toString()),
+                        Color.parseColor("#ff00ff")));
+        pieChart.addPieSlice(
+                new PieModel(
+                        "tvSuKien",
+                        Integer.parseInt(tvSuKien.getText().toString()),
+                        Color.parseColor("#ff00ff")));
+        pieChart.addPieSlice(
+                new PieModel(
+                        "tvKhac",
+                        Integer.parseInt(tvKhac.getText().toString()),
+                        Color.parseColor("#ff00ff")));
+
+        pieChart.startAnimation();
+
     }
 }
--- app/src/main/java/com/example/moneymanager/Fragment/ThongKe/ThongKe_ThuFragment.java
+++ app/src/main/java/com/example/moneymanager/Fragment/ThongKe/ThongKe_ThuFragment.java
@@ -1,6 +1,7 @@
 package com.example.moneymanager.Fragment.ThongKe;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -11,6 +12,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.anychart.AnyChart;
 import com.anychart.AnyChartView;
@@ -22,12 +24,18 @@ import com.anychart.enums.Align;
 import com.anychart.chart.common.listener.Event;
 import com.anychart.chart.common.listener.ListenersInterface;
 import com.example.moneymanager.Adapter.FragmentThuChiAdaper;
+import com.example.moneymanager.Database.DatabaseHelper;
+import com.example.moneymanager.Model.LoaiThu;
+import com.example.moneymanager.Model.Thu;
 import com.example.moneymanager.R;
 import com.example.moneymanager.Them_ThuChi;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
 
+
+import org.eazegraph.lib.charts.PieChart;
+import org.eazegraph.lib.models.PieModel;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -37,21 +45,29 @@ public class ThongKe_ThuFragment extends Fragment {
 
     private RecyclerView recyclerView;
     private AnyChartView anyChartView;
-
+    TextView tvLuong, tvThuong, tvThemGio, tvKhac;
+    PieChart pieChart;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_thu_chi, container, false);
+        return inflater.inflate(R.layout.fragment_thong_ke__thu, container, false);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        recyclerView = view.findViewById(R.id.frag_recyclerView_bieudo_thu);
-        anyChartView = view.findViewById(R.id.bieudo_thu);
+        tvLuong=view.findViewById(R.id.tvLuong);
+        tvThuong=view.findViewById(R.id.tvThuong);
+        tvThemGio=view.findViewById(R.id.tvThemGio);
+        tvKhac=view.findViewById(R.id.tvKhac);
+        pieChart = view.findViewById(R.id.piechart_thu);
+        setData();
+
+        //recyclerView = view.findViewById(R.id.frag_recyclerView_bieudo_thu);
+        // = view.findViewById(R.id.bieudo_thu);
 
 //        Pie pie = AnyChart.pie();
 //
@@ -82,5 +98,58 @@ public class ThongKe_ThuFragment extends Fragment {
 
     }
 
+    private void setData(){
+        DatabaseHelper db= new DatabaseHelper(getContext());
+        List<Thu> list = new ArrayList<>();
+        list=db.getAllThu();
+        int luong=0;
+        int thuong=0;
+        int themgio=0;
+        int khac=0;
+
+        for(int i=0;i<list.size();i++){
+            if(list.get(i).getLoaiThu().getId()==db.getLoaiThuByName("Tiền lương").getId()){
+                luong+= list.get(i).getSotien();
+            }
+            if(list.get(i).getLoaiThu().getId()==db.getLoaiThuByName("Tiền thưởng").getId()){
+                thuong+= list.get(i).getSotien();
+            }
+            if(list.get(i).getLoaiThu().getId()==db.getLoaiThuByName("Trả thêm giờ").getId()){
+                themgio+= list.get(i).getSotien();
+            }
+            if(list.get(i).getLoaiThu().getId()==db.getLoaiThuByName("Khác").getId()){
+                khac+= list.get(i).getSotien();
+            }
+        }
+        tvLuong.setText(Integer.toString(luong));
+        tvThuong.setText(Integer.toString(thuong));
+        tvThemGio.setText(Integer.toString(themgio));
+        tvKhac.setText(Integer.toString(khac));
+
+
+        pieChart.addPieSlice(
+                new PieModel(
+                        "Lương",
+                        Integer.parseInt(tvLuong.getText().toString()),
+                        Color.parseColor("#ff0000")));
+        pieChart.addPieSlice(
+                new PieModel(
+                        "Thưởng",
+                        Integer.parseInt(tvThuong.getText().toString()),
+                        Color.parseColor("#0000ff")));
+        pieChart.addPieSlice(
+                new PieModel(
+                        "Trả thêm giờ",
+                        Integer.parseInt(tvThemGio.getText().toString()),
+                        Color.parseColor("#ffff00")));
+        pieChart.addPieSlice(
+                new PieModel(
+                        "Khác",
+                        Integer.parseInt(tvKhac.getText().toString()),
+                        Color.parseColor("#ff00ff")));
+
+        pieChart.startAnimation();
+
+    }
 
 }
